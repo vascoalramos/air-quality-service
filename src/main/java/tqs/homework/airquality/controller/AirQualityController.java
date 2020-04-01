98d259b9e40e6f0260c41a4e07487ea882e0dedb
Vasco Ramos <vascoalramos@outlook.pt>
2020-04-01 21:37:05 +0100

    ✨ Add minor controller

--- src/main/java/tqs/homework/airquality/controller/AirQualityController.java
+++ src/main/java/tqs/homework/airquality/controller/AirQualityController.java
@@ -1,10 +1,22 @@
 package tqs.homework.airquality.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Vasco Ramos
  * @date 01/04/20
  * @time 17:34
  */
 
+@RestController
 public class AirQualityController {
+
+
+    @GetMapping("/air-metrics")
+    public ResponseEntity<String> getAllCities() {
+        return new ResponseEntity<>("Metrics", HttpStatus.OK);
+    }
 }
