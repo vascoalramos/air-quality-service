211f5934d5ccb26cd9dd7db0a5ab07c0bb138fbc
Vasco Ramos <vascoalramos@outlook.pt>
2020-04-02 21:52:12 +0100

    ✅ Add more tests

--- src/test/java/tqs/homework/airquality/controller/CityControllerTest.java
+++ src/test/java/tqs/homework/airquality/controller/CityControllerTest.java
@@ -11,6 +11,7 @@ import tqs.homework.airquality.model.City;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.reset;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -26,10 +27,23 @@ public class CityControllerTest {
     private MockMvc servlet;
 
     @Test
-    public void whenGetCity_thenReturnCity() throws Exception {
+    public void whenGetCities_thenReturnCities() throws Exception {
         servlet.perform(get("/cities").contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray());
+    }
 
+    @Test
+    public void whenGetCitiesValidQuery_thenReturnCities() throws Exception {
+        servlet.perform(get("/cities?q=vise").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray());
+    }
 
+    @Test
+    public void whenGetCitiesInvalidQuery_thenReturnEmptyCities() throws Exception {
+        servlet.perform(get("/cities?q=1912192_q").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isEmpty());
     }
 }
